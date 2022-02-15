3d21fad3700b6e68d2fcb844d5b1ac459acbbee0
elonim <54721094+elonim@users.noreply.github.com>
2022-02-15 14:24:39 +0100

    Scatter Matrix

--- PandasTest/main.py
+++ PandasTest/main.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 def header(msg):
     print('-' * 50)
     print('[' + msg + ']')
+    print('-' * 50)
 
 header("Load Data fra CSV")
 
@@ -21,3 +22,18 @@ print(df.describe())
 
 header("Print Shape")
 print(df.shape)
+
+sm = pd.plotting.scatter_matrix(df, alpha=0.2, figsize=(6, 6), diagonal='kde')
+
+#Change label rotation
+[s.xaxis.label.set_rotation(45) for s in sm.reshape(-1)]
+[s.yaxis.label.set_rotation(0) for s in sm.reshape(-1)]
+
+#May need to offset label when rotating to prevent overlap of figure
+[s.get_yaxis().set_label_coords(-0.3,0.5) for s in sm.reshape(-1)]
+
+#Hide all ticks
+[s.set_xticks(()) for s in sm.reshape(-1)]
+[s.set_yticks(()) for s in sm.reshape(-1)]
+
+plt.show()
